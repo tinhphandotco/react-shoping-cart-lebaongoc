059eb944f540e0896fde896b075f6517e1a4bcc9
Ngoc <ngocqh0601@gmail.com>
2020-02-24 11:55:30 +0700

    update baitap

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -4,13 +4,13 @@ import "./Product.css"
 import ProductItem from "./ProductItem"
 
 const maxItem = 23;
-const minItem =0;
+const minItem = 0;
 export class Product extends Component {
   constructor() {
     super();
     this.state = {
       products: [],
-      page:1,
+      page: 1,
     };
   }
   componentDidMount() {
@@ -21,53 +21,54 @@ export class Product extends Component {
       .then(item => {
         this.setState({ products: item.data });
       })
-      .catch( function error(){
-        var Error= 
-        '<div class="err"> ERROR: LOI KHONG LAY DUOC DU LIEU </div>';
-         document.getElementById('root').innerHTML = Error;
-    }
+      .catch(function error() {
+        var Error =
+          '<div class="err"> ERROR: LOI KHONG LAY DUOC DU LIEU </div>';
+        document.getElementById('root').innerHTML = Error;
+      }
       );
   }
   loadMore() {
-    let isPage = this.state.page ;
+    let isPage = this.state.page;
     axios({
       method: "get",
-      url: `http://localhost:3001/users?_page=${isPage+1}&_limit=4`
+      url: `http://localhost:3001/users?_page=${isPage + 1}&_limit=4`
     })
       .then(item => {
         this.setState({ products: this.state.products.concat(item.data) });
-        this.state.page++
+        this.state.page ++
+        console.log(this.state.page)
       })
-      .catch(function error(){
-        var Error= 
-        '<div class="err"> ERROR: BI NGAT KET NOI VOI SERVER </div>';
-         document.getElementById('root').innerHTML = Error;
-    });
+      .catch(function error() {
+        var Error =
+          '<div class="err"> ERROR: BI NGAT KET NOI VOI SERVER </div>';
+        document.getElementById('root').innerHTML = Error;
+      });
   }
 
   render() {
     const product = this.state.products.map(product => {
       return (
-        <ProductItem key={product.id} product={product}></ProductItem>
+        <ProductItem key={product.id} product={product}/>
       )
-  })
-  return(
-    <div>
-      <div className="wrap">
-        {product}
+    })
+    return (
+      <div>
+        <div className="wrap">
+          {product}
+        </div>
+        <div className="footer">
+          <button
+            style={{ display: this.state.products.length >= maxItem || this.state.products.length === minItem ? 'none' : 'block' }}
+            className="btnLoadMore"
+            onClick={() => this.loadMore()}
+          >
+            LoadMore
+          </button>
+        </div>
       </div>
-      <div className="wrap2">
-      <button
-        style={{ display: this.state.products.length >= maxItem || this.state.products.length === minItem ? 'none' : 'block' }} 
-        className="btnLoadMore"
-        onClick={() => this.loadMore()}
-      >
-        LoadMore
-      </button>
-    </div>
-  </div>
-  )
-}
+    )
+  }
 }
 
 export default Product;
--- src/Product/ProductItem.jsx
+++ src/Product/ProductItem.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 export class ProductItem extends Component{
     render() {
       return (
